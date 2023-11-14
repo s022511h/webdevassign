cdadfa177cd70fb6afc79baa2d2ec9cc713573d2
s022511h <61841001+s022511h@users.noreply.github.com>
2023-11-14 16:33:39 +0000

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["*.{html,js}"],
+  important: true,
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./sections/**/*.{js,ts,jsx,tsx}",
+
+    "./App/**/*.{js,ts,jsx,tsx}",
+    "./Pages/**/*.{js,ts,jsx,tsx}",
+    "./Components/**/*.{js,ts,jsx,tsx}",
+    "./Sections/**/*.{js,ts,jsx,tsx}",
+
+    // Or if using `src` directory:
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ]
   theme: {
     screens: {
       
